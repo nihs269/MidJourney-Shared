2040827d9d9e9933a8ea69c90f6633e26fcf8219
Trouble <nihs269@gmail.com>
2023-05-10 09:10:36 +0700

    Initial commit

---
+++ Globals.py
@@ -0,0 +1,14 @@
+# strings
+
+DAVINCI_TOKEN = "Your Bot's token"
+SALAI_TOKEN = "Your token"
+SERVER_ID = "ID Server"
+CHANNEL_ID = "ID Chanel"
+
+# boolean
+USE_MESSAGED_CHANNEL = False
+
+# don't edit the following variable
+MID_JOURNEY_ID = "936929561302675456"  # midjourney bot id
+targetID = ""
+targetHash = ""
